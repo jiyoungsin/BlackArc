c7203d9256194144dc86298bc9518f159ce7f237
Paul Sin <jiyoungsin97@gmail.com>
2022-03-07 17:24:40 -0500

    learning about targets and how masks work.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,100 +1,39 @@
-const app = new PIXI.Application({
-    width: 1500,
-    height: 1500,
-    backgroundColor: 0x1099bb,
-    antialias: true
-})
+const app = new PIXI.Application();
 document.body.appendChild(app.view);
 
-// set interactive true
 app.stage.interactive = true;
 
-// grabbing the image
-const bg = PIXI.Sprite.from('assets/assets/bg_rotate.jpg');
+const bg = PIXI.Sprite.from('assets/assets/bg_plane.jpg');
 
-// Setting rotation Anchor point.
-bg.anchor.set(0.5);
-
-// setting the location of the image.
-bg.x = app.screen.width / 2;
-bg.y = app.screen.height / 2;
-
-// add the BG object.
 app.stage.addChild(bg);
 
-// Making an object/Container to allow for adding of other objects.
-const container = new PIXI.Container();
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
+const cells = PIXI.Sprite.from('assets/assets/cells.png');
 
-// add a bunch of sprites
-const bgFront = PIXI.Sprite.from('assets/assets/bg_scene_rotate.jpg');
-bgFront.anchor.set(0.5);
+cells.scale.set(1.5);
 
-const light2 = PIXI.Sprite.from('assets/assets/light_rotate_2.png');
-light2.anchor.set(0.5);
+const mask = PIXI.Sprite.from('assets/assets/flowerTop.png');
+mask.anchor.set(0.5);
+mask.x = 310;
+mask.y = 190;
 
-const light1 = PIXI.Sprite.from('assets/assets/light_rotate_1.png');
-light1.anchor.set(0.5);
+cells.mask = mask;
 
-const panda = PIXI.Sprite.from('assets/assets/panda.png');
-panda.anchor.set(0.5);
+app.stage.addChild(mask, cells);
 
-container.addChild(bgFront, light2, light1, panda);
-// adding container to the app.
-app.stage.addChild(container);
+const target = new PIXI.Point();
 
-// let's create a moving shape
-const thing = new PIXI.Graphics();
-app.stage.addChild(thing);
-thing.x = app.screen.width / 2;
-thing.y = app.screen.height / 2;
-thing.lineStyle(1);
+reset();
 
-container.mask = thing;
+function reset() {
+    target.x = Math.floor(Math.random() * 550);
+    target.y = Math.floor(Math.random() * 300);
+}
 
-// toggling the masking.
-app.stage.on('pointertap', () => {
-    if (!container.mask) {
-        container.mask = thing;
-    } else {
-        container.mask = null;
-    }
-});
-
-//adding some text to the stage.
-const help = new PIXI.Text('Click or tap to turn masking on / off.', {
-    fontFamily: 'Arial',
-    fontSize: 12,
-    fontWeight: 'bold',
-    fill: 'white',
-});
-help.y = app.screen.height - 26;
-help.x = 10;
-app.stage.addChild(help);
-
-// loop for the elements inside.
-let count = 0;
 app.ticker.add(() => {
-    bg.rotation += 0.01;
-    bgFront.rotation -= 0.01;
+    mask.x += (target.x - mask.x) * 0.1;
+    mask.y += (target.y - mask.y) * 0.1;
 
-    light1.rotation += 0.02;
-    light2.rotation += 0.01;
-
-    // using sin and cos to make the image go bigger and smaller
-    // increase 0.04 to increase the magnitude.
-    panda.scale.x = 1 + Math.sin(count) * 0.04;
-    panda.scale.y = 1 + Math.cos(count) * 0.04;
-
-    count += 0.1;
-
-    thing.clear();
-
-    thing.beginFill(0x8bc5ff, 0.4);
-    thing.moveTo(-120 + Math.sin(count) * 20, -100 + Math.cos(count) * 20);
-    thing.lineTo(120 + Math.cos(count) * 20, -100 + Math.sin(count) * 20);
-    thing.lineTo(120 + Math.sin(count) * 20, 100 + Math.cos(count) * 20);
-    thing.lineTo(-120 + Math.cos(count) * 20, 100 + Math.sin(count) * 20);
-    thing.rotation = count * 0.1;
+    if (Math.abs(mask.x - target.x) < 1) {
+        reset();
+    }
 });
