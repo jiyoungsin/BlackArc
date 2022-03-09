c8d8bcf16a7ce1caa52cb1a125dbfb5ca824c82d
Paul Sin <jiyoungsin97@gmail.com>
2022-03-09 17:32:53 -0500

    learning displacement

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,97 +1,129 @@
-const app = new PIXI.Application();
+const app = new PIXI.Application({
+    width: 1250,
+    height: 1250,
+    backgroundColor: 0x1099bb,
+    antialias: true
+})
 document.body.appendChild(app.view);
 
-// setting interactive true
+// set interactive to true
 app.stage.interactive = true;
 
-// getting the background 
-const bg = PIXI.Sprite.from('assets/assets/bg_rotate.jpg');
-// set the anchor to the middle
-bg.anchor.set(0.5);
-// set the location of the anchor
-bg.x = app.screen.width / 2;
-bg.y = app.screen.height / 2;
-
-// creating a color matrix
-const filter = new PIXI.filters.ColorMatrixFilter();
-
-// creating a container
+// creating a container 
 const container = new PIXI.Container();
-// containers contain objects. 
-// specifying position of container.
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
-
-const bgFront = PIXI.Sprite.from('assets/assets/bg_scene_rotate.jpg');
-// adding the anchor to the middle of the image.
-bgFront.anchor.set(0.5);
-// adding the image to the container
-container.addChild(bgFront);
-
-const light2 = PIXI.Sprite.from('assets/assets/light_rotate_2.png');
-// adding the anchor to the middle of the image.
-light2.anchor.set(0.5);
-// adding the image to the container
-container.addChild(light2);
-
-const light1 = PIXI.Sprite.from('assets/assets/light_rotate_1.png');
-// adding the anchor to the middle of the image.
-light1.anchor.set(0.5);
-// adding the image to the container
-container.addChild(light1);
-
-const panda = PIXI.Sprite.from('assets/assets/panda.png');
-// adding the anchor to the middle of the image.
-panda.anchor.set(0.5);
-// adding the image to the container
-container.addChild(panda);
-
+// adding container to stage.
 app.stage.addChild(container);
 
-// adding the filter to the entire Stage.
-app.stage.filters = [filter];
+// setting some constants
+const padding = 100;
+// creating a rectangle ( bounds )
+const bounds = new PIXI.Rectangle(
+    -padding,
+    -padding,
+    app.screen.width + padding * 2,
+    app.screen.height + padding * 2,
+);
+// creating maggots array
+const maggots = [];
+
+// creating 20 maggots
+for (let i = 0; i < 20; i++) {
+    const maggot = PIXI.Sprite.from('assets/assets/maggot.png');
+    // set the anchor to the center
+    maggot.anchor.set(0.5);
+    // adding the maggot to the container
+    container.addChild(maggot);
+
+    // seting the direction, speed, and turning speed.
+    maggot.direction = Math.random() * Math.PI * 2;
+    // setting the speed
+    maggot.speed = 1;
+    // setting the turn speed.
+    maggot.turnSpeed = Math.random() - 0.8;
+
+    // setting the bounds for each maggot
+    maggot.x = Math.random() * bounds.width;
+    maggot.y = Math.random() * bounds.height;
+
+    // making some maggots bigger and smaller
+    maggot.scale.set(1 + Math.random() * 0.3);
+    // creating an original point for the maggots
+    maggot.original = new PIXI.Point();
+    maggot.original.copyFrom(maggot.scale);
+    // push the maggot to the array
+    maggots.push(maggot);
+}
+
+// creating a displacement sprite
+const displacementSprite = PIXI.Sprite.from('assets/assets/pixi-filters/displace.png');
+// adding a sprite to the filter
+const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+
+app.stage.addChild(displacementSprite);
+// adding the filter to the container.
+container.filters = [displacementFilter];
+
+displacementFilter.scale.x = 110;
+displacementFilter.scale.y = 110;
+displacementSprite.anchor.set(0.5);
+
+const ring = PIXI.Sprite.from('assets/assets/pixi-filters/ring.png');
+
+ring.anchor.set(0.5);
+
+ring.visible = false;
+
+app.stage.addChild(ring);
+
+// adding the grass background.
+const bg = PIXI.Sprite.from('assets/assets/bg_grass.jpg');
+bg.width = app.screen.width;
+bg.height = app.screen.height;
+
+// setting the alpha
+bg.alpha = 0.4;
+
+// adding the bg to the container
+container.addChild(bg);
+
+app.stage
+    .on('mousemove', onPointerMove)
+    .on('touchmove', onPointerMove);
+
+// when the cursor moves handler
+function onPointerMove(eventData) {
+    ring.visible = true;
+
+    displacementSprite.position.set(eventData.data.global.x - 25, eventData.data.global.y);
+    ring.position.copyFrom(displacementSprite.position);
+}
 
 let count = 0;
-let enabled = true;
 
-// toggling the ColorMatrixFilter.
-app.stage.on('pointertap', () => {
-    enabled = !enabled;
-    app.stage.filters = enabled ? [filter] : null;
-});
+app.ticker.add(() => {
+    count += 0.05;
 
-// Pixi Text. 
-const help = new PIXI.Text('Click or tap to turn filters on / off.', {
-    fontFamily: 'Arial',
-    fontSize: 12,
-    fontWeight: 'bold',
-    fill: 'white',
-});
-// setting position.
-help.y = app.screen.height - 25;
-help.x = 10;
-// adding the text.
-app.stage.addChild(help);
-// const { matrix } = filter;
-console.log(filter.matrix);
-app.ticker.add((delta) => {
-    bg.rotation += 0.001;
-    bgFront.rotation -= 0.001;
-    light1.rotation += 0.002;
-    light2.rotation += 0.001;
+    for (let i = 0; i < maggots.length; i++) {
+        const maggot = maggots[i];
 
-    panda.scale.x = 1 + Math.sin(count) * 0.004;
-    panda.scale.y = 1 + Math.cos(count) * 0.004;
+        maggot.direction += maggot.turnSpeed * 0.01;
+        maggot.x += Math.sin(maggot.direction) * maggot.speed;
+        maggot.y += Math.cos(maggot.direction) * maggot.speed;
 
-    count += 0.1;
+        maggot.rotation = -maggot.direction - Math.PI / 2;
+        maggot.scale.x = maggot.original.x + Math.sin(count) * 0.2;
 
-    // this matrix should be responsible for this filter.
-    const { matrix } = filter;
-    // console.log(matrix);
-    matrix[1] = Math.sin(count) * 3;
-    matrix[2] = Math.cos(count);
-    matrix[3] = Math.cos(count) * 1.5;
-    matrix[4] = Math.sin(count / 3) * 2;
-    matrix[5] = Math.sin(count / 2);
-    matrix[6] = Math.sin(count / 4);
+        // wrap the maggots around as the crawl
+        if (maggot.x < bounds.x) {
+            maggot.x += bounds.width;
+        } else if (maggot.x > bounds.x + bounds.width) {
+            maggot.x -= bounds.width;
+        }
+
+        if (maggot.y < bounds.y) {
+            maggot.y += bounds.height;
+        } else if (maggot.y > bounds.y + bounds.height) {
+            maggot.y -= bounds.height;
+        }
+    }
 });
